d136886c2c77ffd7ea5d2d139ccaeed65304e70a
lara ahmad <84351032+laraahmad01@users.noreply.github.com>
2023-05-11 20:52:48 +0300

    roles create

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -7,9 +7,26 @@ use App\Models\Roles;
 
 class RolesController extends Controller
 {
-    public function Index(){
+    public function store(Request $request)
+    {
+        // Validate the request data
+        $request->validate([
+            'name' => 'required|unique:roles|max:255',
+        ]);
 
-        return view('Index');
+        // Create a new role instance
+        $role = new Roles;
+        $role->name = $request->name;
+        $role->save();
 
+        return redirect()->route('roles.index')->with('success', 'Role added successfully');
+    }
+
+    public function index()
+    {
+        // Retrieve all roles from the database
+        $roles = Roles::all();
+
+        return view('roles.index', compact('roles'));
     }
 }
--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -1,4 +1,15 @@
 <!DOCTYPE html>
-<body>
-    <p>Hello world</p>
-</body>
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($roles as $role)
+        <tr>
+            <td>{{ $role->name }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
