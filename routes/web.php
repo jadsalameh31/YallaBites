3afb7aa67e9683fd7fe5a7c075ff93b89ad268a8
lara ahmad <84351032+laraahmad01@users.noreply.github.com>
2023-05-11 20:21:53 +0300

    Roles commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RolesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', function () {
     return view('welcome');
+});
+
+Route::get('Index', function(){
+    return view('Index');
 });
